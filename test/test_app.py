5e0def75ebc6db90d605bdc5eee19bca4dc19ffb
mkfc13 <mkfc13@gmail.com>
2022-06-08 17:42:44 +0100

    updated test_app by marshall

--- test/test_app.py
+++ test/test_app.py
@@ -17,3 +17,46 @@ try:
 
     cur = conn.cursor()
 
+    create_items_table = '''  CREATE TABLE items_table (
+                            item_id	        SERIAL NOT NULL PRIMARY KEY,	
+                            item_name	    varchar	NOT NULL,
+                            item_size	    varchar	NOT NULL,	
+                            item_flavor     varchar NULL,	
+                            item_price		numeric NOT NULL)'''
+                            
+    create_payment_table = '''
+                            CREATE TABLE Payment
+                            (
+                            Order_id	    integer,
+                            Total_Price	    numeric	NOT NULL,
+                            Date        	date	NOT NULL,	
+                            Time        	time	NOT NULL,	
+                            Day_of_the_Week	numeric NOT NULL,
+                            Cash_or_Card	varchar	NOT NULL,	
+                            Card_Number	    varchar
+                            );
+                            '''
+                            
+    cur.execute(create_payment_table)
+    
+    create_customer_table = '''
+                            CREATE TABLE Customer
+                            (
+                            Customer_id	    SERIAL NOT NULL,
+                            Customer_Name	varchar	NOT NULL,
+                            Store_id        interger
+                            );
+                            '''
+
+    cur.execute(create_items_table, create_payment_table, create_customer_table)
+
+    conn.commit()
+
+except Exception as error:
+    print(error)
+
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
