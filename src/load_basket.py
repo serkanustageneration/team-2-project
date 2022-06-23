71105a0891993bd613f2829dc513d2eff03f5de2
mkfc13 <mkfc13@gmail.com>
2022-06-23 10:18:31 +0100

    Updated to work once

--- src/load_basket.py
+++ src/load_basket.py
@@ -3,7 +3,16 @@ import numpy as np
 import psycopg2.extras as extras
 
 from extract import *
-  
+
+basket_df = create_basket_df()
+order_df = create_orders_df()
+customer_id = order_df['cust_id']
+store_id = order_df['branch_id']
+time_stamp = data['timestamp']
+
+basket_df['customer_id'] = customer_id
+basket_df['store_id'] = store_id
+basket_df['time_stamp'] = time_stamp  
   
 def execute_values(conn, df, table):
   
@@ -29,7 +38,5 @@ conn = psycopg2.connect(
     database="team-2_group-project", user='root', password='pass', host='127.0.0.1', port='5432'
 )
 
-execute_values(conn, customer_df, 'customer_df')
-execute_values(conn, products_df, 'products_df')
-execute_values(conn, store_df, 'store_df')
+execute_values(conn, basket_df, 'basket_df')
 
